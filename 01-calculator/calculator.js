8a80e27fedd2472411d6904da8910f470915e1e6
Moshe Gelberg <glbrgm@gmail.com>
2019-10-28 14:42:15 +0200

    Calculator - ver 1.0

--- 01-calculator/calculator.js
+++ 01-calculator/calculator.js
@@ -1,27 +1,5 @@
 "use strict";
 
-f/*unction renderExpression() {
-    let number = '';
-    let numArr = [];
-    const display = document.getElementById('display');
-    addEventListener('click', event => {
-        const button = event.target;
-        if (button.tagName === 'BUTTON') {
-            const action = button.dataset.action;
-            if (display.innerHTML === '0') {
-                display.innerHTML = '';
-            }
-            const buttonHTML = button.innerHTML;
-            if (Number.isInteger(+buttonHTML)) {
-                display.innerHTML += buttonHTML;
-            } else {
-                display.innerHTML += renderOperator(action);
-            }
-
-        }
-    });
-}*/
-
 function renderOperator(operator) {
     switch (operator) {
         case 'add':
@@ -43,55 +21,59 @@ function renderOperator(operator) {
     }
 }
 
-function calculate(arr) {
-
-}
-
 const math = {
-    add: {
-        precedence: 1,
-        operation: function (x, y) {
-            return x + y;
-        }
+    add(x, y) {
+        return x + y;
     },
 
-    subtract: {
-        precedence: 1,
-        operation: function (x, y) {
-            return x - y;
-        }
+    subtract(x, y) {
+        return x - y;
     },
 
-    multiply: {
-        precedence: 2,
-        operation: function (x, y) {
-            return x * y;
-        }
+    multiply(x, y) {
+        return x * y;
     },
 
-    divide: {
-        precedence: 2,
-        operation: function (x, y) {
-            return x * y;
-        },
-        isInvalidInput: function (x, y) {
-            return y === 0 ? 'Division by 0!' : false;
-        }
-
+    divide(x, y) {
+        return y !== 0 ? x / y : 'Division by 0!';
     },
 
-    squareRoot: {
-        precedence: 3,
-        operation: function (x) {
-            return Math.sqrt(x);
-        }
+    squareRoot(x) {
+        return Math.sqrt(x);
     },
 
-    squared: {
-        precedence: 3,
-        operation: function (x) {
-            return Math.pow(x, 2);
+    squared(x) {
+        return Math.pow(x, 2);
+    },
+
+    calculate(numArr) {
+        let total = 0;
+        while (numArr[1] !== '=') {
+            const firstOperand = numArr[0];
+            const sign = numArr[1];
+            const secondOperand = numArr[2];
+
+            switch (sign) {
+                case '+':
+                    total = this.add(firstOperand, secondOperand);
+                    break;
+                case '-':
+                    total = this.subtract(firstOperand, secondOperand);
+                    break;
+                case '*':
+                    total = this.multiply(firstOperand, secondOperand);
+                    break;
+                case "÷":
+                    total = this.divide(firstOperand, secondOperand);
+                    break;
+                // case 'squared':
+                //     return;
+                // case 'square-root':
+                //     return;
+            }
+            numArr.splice(0, 3, total);
         }
+        return numArr[0];
     }
 };
 
@@ -99,36 +81,55 @@ const auxiliaryOperation = {
     display: document.getElementById('display'),
 
     clear() {
-        this.display.innerHTML = '';
+        this.display.innerText = '0';
     },
 
     backspace() {
         this.display.innerText = this.display.innerText.slice(0, -1);
+    },
+
+    renderResult(result) {
+        this.display.innerText = result;
     }
 };
 
 window.onload = () => {
-    // renderExpression();
     const display = document.getElementById('display');
+    let numArray = [];
+    let numString = '';
     addEventListener('click', event => {
+        // debugger;
         const button = event.target;
         if (button.tagName === 'BUTTON') {
             const action = button.dataset.action;
-            if (display.innerHTML === '0') {
+            const buttonContent = button.innerText;
+            if ((display.innerHTML === '0') || numArray[1] === '=') {
+                numArray = [];
                 display.innerHTML = '';
             }
-            const buttonHTML = button.innerHTML;
-            if (Number.isInteger(+buttonHTML)) {
-                display.innerHTML += buttonHTML;
-            } else {
+            if (!action) {
+                display.innerHTML += buttonContent;
+                numString += buttonContent;
+            } else if (action !== 'backspace') {
                 display.innerHTML += renderOperator(action);
+                numArray.push(+numString);
+                numString = '';
+                numArray.push(buttonContent);
             }
             switch (action) {
-
-
+                case 'clear':
+                    auxiliaryOperation.clear();
+                    break;
+                case 'backspace':
+                    auxiliaryOperation.backspace();
+                    numString = numString.slice(0, -1);
+                    break;
+                case 'calculate':
+                    const total = math.calculate(numArray);
+                    auxiliaryOperation.renderResult(total);
+                    break;
             }
         }
-
     });
 
 };
