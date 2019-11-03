99f476373072ee4386f566db9fb4811f8e33a73d
Moshe Gelberg <glbrgm@gmail.com>
2019-11-03 22:13:12 +0200

    Paternal class Data has been added

---
+++ 05-travel/js/Data.js
@@ -0,0 +1,17 @@
+class Data {
+    constructor(source, container) {
+        console.log('Data.constructor() start | container:', container);
+        this.container = container;
+    }
+
+    _getDataHTML(data, template) {
+        let dataHTML = '<tr>';
+        data.forEach(dataItem => {
+            for (const templateItem of template) {
+                dataHTML += `<td>${templateItem}</td>`.replace(templateItem, dataItem[templateItem]);
+            }
+            dataHTML += '</tr>';
+        });
+        return dataHTML;
+    }
+}
