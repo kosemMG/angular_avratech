2dd49238d10a38d00cb88568f8488a5da50c8d63
Moshe Gelberg <glbrgm@gmail.com>
2019-11-03 22:14:11 +0200

    Class Orders extends Data has been added

---
+++ 05-travel/js/Orders.js
@@ -0,0 +1,16 @@
+class Orders extends Data {
+    constructor(container = 'orders') {
+        super();
+        this.template = ['id', 'name', 'personalID', 'travelID', 'passengersNumber', 'total'];
+        this.orders = [];
+        this._init();
+    }
+
+    _init() {
+        this.ordersTable = document.getElementById(this.container);
+    }
+
+    _render() {
+        this.ordersTable.innerHTML = this._getDataHTML(this.orders, this.template);
+    }
+}
