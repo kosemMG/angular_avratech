79a257f2fd09f5f030187d3b24e7784646e57b54
Moshe Gelberg <glbrgm@gmail.com>
2019-11-03 22:12:03 +0200

    Class Destinations extends Data

---
+++ 05-travel/js/Destinations.js
@@ -0,0 +1,40 @@
+class Destinations extends Data {
+    constructor(source, container = 'destinations') {
+        console.log('Destinations.constructor() start | source:', source, 'container:', container);
+        super(source, container);
+        this.source = source;
+        this.template = ['id', 'name', 'price'];
+        this.destinations = [];
+        this._init(this.source);
+    }
+
+    _init(source) {
+        console.log('Destinations._init() start source:', source);
+        this.destinationsTable = document.getElementById(this.container);
+
+        fetch(source)
+            .then(result => result.json())
+            .then(data => {
+                for (let destination of data.destinations) {
+                    this.destinations.push(destination);
+                }
+            })
+            .then(() => {
+                this.destinationsTable.innerHTML = this._getDataHTML(this.destinations, this.template);
+            });
+    }
+
+    // _render(destinations) {
+    //     let destinationRowTemplate = '<tr><td>#ID</td><td>#NAME</td><td>#PRICE</td></tr>';
+    //     let destinationsHTML = '';
+    //
+    //     destinations.forEach(destination => {
+    //         destinationsHTML += destinationRowTemplate
+    //             .replace('#ID', destination.id)
+    //             .replace('#NAME', destination.name)
+    //             .replace('#PRICE', destination.price)
+    //     });
+    //
+    //     this.destinationsTable.innerHTML = destinationsHTML;
+    // }
+}
